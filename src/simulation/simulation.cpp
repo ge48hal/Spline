a0addde0601b7e50306f55b3f6d02876d3b0b2bd
JONGTAE <jongtae@RE220.local>
2025-12-14 18:51:08 +0100

    cmake bug fixed ( Didn't worked well on intel mac)

--- src/simulation/simulation.cpp
+++ src/simulation/simulation.cpp
@@ -1,5 +1,11 @@
 #include "simulation/simulation.h"
-#include <omp.h>
+
+
+#if OMP 
+    include <omp.h>
+#endif
+
+//#include <omp.h>
 
 
 std::vector<double> computeAreaTimeslices(std::span<const Points> slices)
@@ -7,7 +13,9 @@ std::vector<double> computeAreaTimeslices(std::span<const Points> slices)
     const std::size_t size = slices.size();
     std::vector<double> result(size);
 
-    #pragma omp parallel for 
+    #if OMP
+        #pragma omp parallel for
+    #endif
     // Use dynamic scheduling to balance workload across threads
     for(size_t i = 0; i< size; ++i){
         result[i] = geom::Shoelace::calculateArea(slices[i]);
@@ -21,7 +29,9 @@ std::vector<double> computeMomentumTimeslices(std::span<const Points> slices)
     const std::size_t size = slices.size();
     std::vector<double> result(size);
 
-    #pragma omp parallel for 
+    #if OMP
+        #pragma omp parallel for
+    #endif
     // Use dynamic scheduling to balance workload across threads
     for(size_t i = 0; i< size; ++i){
         result[i] = geom::Shoelace::calculateMomentum(slices[i]);
